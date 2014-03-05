92f61b172f9f015146465497348e92939541dc5d
smoore8 <savionmoore8@gmail.com>
2014-03-05 10:10:57 -0500

    NumbGuess

---
+++ NumbGuess/NumbGuess/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumbGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int low = 0, high = 100;
+            int guess;
+
+            while (true)
+            {
+                guess = high/=;
+                Console.WriteLine("Is your guess {0}? h,l,e ", guess);
+
+                string input = Console.ReadLine();
+            }
+        }
+    }
+}
