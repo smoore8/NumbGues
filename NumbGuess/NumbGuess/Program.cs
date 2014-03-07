9e58667092fabbf12e3800a9aed6b0c009d94f08
smoore8 <savionmoore8@gmail.com>
2014-03-07 10:13:30 -0500

    winer

--- NumbGuess/NumbGuess/Program.cs
+++ NumbGuess/NumbGuess/Program.cs
@@ -9,16 +9,39 @@ namespace NumbGuess
     {
         static void Main(string[] args)
         {
-            int low = 0, high = 100;
-            int guess;
+            String input;
+
+            int guess, low = 0, high = 100;    
+          
 
             while (true)
             {
-                guess = high + low / 2;
+                guess = (high + low) / 2;
                 Console.WriteLine("Is your guess {0}? h,l,e ", guess);
 
-                string input = Console.ReadLine();
+                input = Console.ReadLine();
+
+                if (input == "h")
+                {
+                    low = guess + 1;
+                }
+                else if (input == "l")
+                {
+                    high = guess + 1;
+                }
+
+
+                 if (input == "e")
+                {
+
+                    Console.WriteLine("I win!!!");
+
+                        System.Environment.Exit(-1);
+                }
+                        
             }
+
         }
+
     }
 }
